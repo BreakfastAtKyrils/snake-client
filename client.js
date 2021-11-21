c2b0d150116b1b854707a3ea4edbe2adf203d293
BreakfastAtKyrils <kyrildr@gmail.com>
2021-11-21 10:04:26 -0500

    client and play

---
+++ client.js
@@ -0,0 +1,15 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost', // IP address here,
+    port: 50541 // PORT number here,
+  });
+
+  conn.setEncoding("utf8");
+  return conn;
+}
+
+module.exports = {
+  connect,
+}
